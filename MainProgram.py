b4eb24a7a45da3f48a02a84a68a1e7f7eeab3277
arila22 <arila.behrami@student.uni-pr.edu>
2025-05-11 21:24:30 +0200

    Update MainProgram.py
    Funksioni për dekriptim me algoritmin Vigenère

--- MainProgram.py
+++ MainProgram.py
@@ -29,3 +29,18 @@ def vigenere_encrypt(text,key):
         else:
             result+=char
     return  result
+
+def vigenere_decrypt(text, key):
+    result = ""
+    key = key.lower()
+    key_index = 0
+    for char in text:
+        if char.isalpha():
+            shift = -(ord(key[key_index % len(key)]) - ord('a'))
+            base = ord('A') if char.isupper() else ord('a')
+            result += chr((ord(char) - base + shift) % 26 + base)
+            key_index += 1
+        else:
+            result += char
+    return result
+
