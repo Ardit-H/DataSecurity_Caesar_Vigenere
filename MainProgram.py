528ae3e1f244a384389380b26467030204a88222
Ardit-H <ardit.hyseni5@student.uni-pr.edu>
2025-05-11 18:39:49 +0200

    Funksioni per enkriptim me ane te algoritmit Vigenère

--- MainProgram.py
+++ MainProgram.py
@@ -15,3 +15,17 @@ def caesar_encrypt(text,key):
 
 def caesar_decrypt(text,key):
     return caesar_encrypt(text,-key)
+
+def vigenere_encrypt(text,key):
+    result=""
+    key=key.lower()
+    key_index=0
+    for char in text:
+        if char.isalpha():
+            shift=ord(key[key_index%len(key)])-ord('a')
+            base=ord('A') if char.isupper() else ord('a')
+            result+=chr((ord(char)-base+shift)%26+base)
+            key_index+=1
+        else:
+            result+=char
+    return  result
