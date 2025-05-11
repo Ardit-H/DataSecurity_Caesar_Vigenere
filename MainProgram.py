eb28562350d7074ca525253272e15b92d7edffbd
Ardit-H <ardit.hyseni5@student.uni-pr.edu>
2025-05-11 18:24:43 +0200

    Funksioni per enkriptim me ane te algoritmit Caesar Chiper

--- MainProgram.py
+++ MainProgram.py
@@ -1,3 +1,14 @@
 import tkinter as tk
 from tkinter import filedialog,messagebox
 import os
+
+def caesar_encrypt(text,key):
+    result = ""
+    for char in text:
+        if char.isalpha():
+            shift = key % 26
+            base = ord('A') if char.isupper() else ord('a')
+            result += chr((ord(char)-base+shift)%26+base)
+        else:
+            result +=char
+    return  result
