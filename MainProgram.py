8e3dc44ae176cf35978d99a7ae8360c107a87513
arila22 <arila.behrami@student.uni-pr.edu>
2025-05-11 21:33:39 +0200

    Update MainProgram.py
     Validon çelësin për algoritmin Vigenere

--- MainProgram.py
+++ MainProgram.py
@@ -50,6 +50,9 @@ def process_file(encrypt=True):
     try:
         if algo == "Caesar":
             key = int(key)
+            elif algo == "Vigenere":
+            if not key.isalpha():
+                raise ValueError("Key for Vigenere must be letters only.")
 
 
 
